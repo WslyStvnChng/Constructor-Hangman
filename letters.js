e04120e2906ed0e400d8579fa959bd4773b02e73
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-02-09 19:17:53 -0700

    connected three files using module export

---
+++ letters.js
@@ -0,0 +1,10 @@
+var letterS = function() {
+
+}
+
+
+
+// Exporting our letters constructor, will require it in words
+module.exports = letterS;
+
+console.log("You are nice");
---
+++ words.js
@@ -0,0 +1,13 @@
+//Must require wordSelector constructor function; exported from words.js
+var letters = require("./letters.js");
+
+//Constructor function for creating Word Objects
+var hiddenWord = function() {
+
+}
+
+
+// Exporting words.js constructor will be needed in main.js
+module.exports = hiddenWord;
+
+console.log("You are the best");
